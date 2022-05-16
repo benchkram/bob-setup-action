ae81f44cfbb5d97489bb921f2004e1067e8a9bf3
Andrei Boar <andrey.boar@gmail.com>
2022-05-16 15:15:04 +0300

    Add action

--- install.sh
+++
@@ -1,3 +0,0 @@
-wget https://github.com/benchkram/bob/releases/download/0.4.3-beta-nix/bob_0.4.3-beta-nix_linux_amd64
-chmod +x bob_0.4.3-beta-nix_linux_amd64
-mv bob_0.4.3-beta-nix_linux_amd64 $(go env GOPATH)/bin/bob
