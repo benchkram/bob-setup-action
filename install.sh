e43eb1a6fbbf407f72846baf2ab19ac23528a4f9
Andrei Boar <andrey.boar@gmail.com>
2022-05-16 14:24:05 +0300

    Add install bob shell script

---
+++ install.sh
@@ -0,0 +1,3 @@
+wget https://github.com/benchkram/bob/releases/download/0.4.3-beta-nix/bob_0.4.3-beta-nix_linux_amd64
+chmod +x bob_0.4.3-beta-nix_linux_amd64
+mv bob_0.4.3-beta-nix_linux_amd64 $(go env GOPATH)/bin/bob
